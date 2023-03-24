064296ae22bf1ea13e7d4fec39715a5238c9efec
Leonid Sulin <sulin.leonid@gmail.com>
2023-03-24 20:12:11 +0300

    Add simple usage example

--- Src/Program.cs
+++ Src/Program.cs
@@ -4,5 +4,8 @@ internal class Program
 {
     public static void Main(string[] args)
     {
+        IFunction function = new RosenbrockFunction();
+        NelderMeadMethod nelderMeadMethod = new NelderMeadMethod(1, 2, 0.5, 0.5);
+        nelderMeadMethod.Run(function, 10);
     }
 }
